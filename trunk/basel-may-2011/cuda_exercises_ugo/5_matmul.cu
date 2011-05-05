9766c61623dea7798ad2f9b8d2b7a351afb41882
uvaretto <uvaretto@45fdcf50-614b-4c32-8fc4-9530904cd465>
2011-05-05 09:20:04 +0000

    Started implementation of driver code

--- trunk/basel-may-2011/cuda_exercises_ugo/5_matmul.cu
+++ trunk/basel-may-2011/cuda_exercises_ugo/5_matmul.cu
@@ -22,8 +22,7 @@ __device__ real_t get_matrix_element( const real_t* m,
                                       int blockRow,
                                       int col,
                                       int row,
-                                      int num_columns ) {
-                                      	
+                                      int num_columns ) {                                      	
   
     return m[ ( blockRow * blockDim.y + row ) * num_columns + blockCol * blockDim.x + col ];
 
@@ -56,7 +55,6 @@ __global__ void matmul_coalesced( const real_t* m1, const real_t* m2, real_t* mo
         __synchthreads(); // required to avoid that some threads start modifying
                           // data in cache before all threads have exited for loop    
     }
-
    mout[ ( blockRow * blockDim.y + row ) * m2_columns + blockCol * blockDim.x + col ] = out;     
 }
 
@@ -71,7 +69,6 @@ __global__ void matmul( const real_t* m1, const real_t* m2, real_t* mout,
     for( int k = 1; k != m1_columns; ++k ) {
     	out += m1[ row * m1_columns + k ] * m2[ k * m2_columns + col ];
     }
-
     mout[ row * m2_columns + col ] = out;
 }
 
@@ -85,6 +82,87 @@ void matmul_ref( const real_t* m1, const real_t* m2, real_t* mout,
     		}
     	}
     }
-
-
 }
+
+bool compare( const real_t* v1, const real_t* v2, size_t N, real_t eps { 
+	for( int i = 0; i != N; ++i ) {
+		if( std::abs( v1[ i ] - v2[ i ] ) > eps ) return false;
+	}
+}
+
+//------------------------------------------------------------------------------
+int main(int argc, char** argv ) {
+	
+	//1024 x 1024 matrices
+	const dim3 BLOCKS( 64, 64 );
+	const dim3 THREADS_PER_BLOCK( 16, 16 ); 
+	const int ROWS = BLOCKS.y * THREADS_PER_BLOCK.y;
+	const int COLUMNS =  BLOCKS.x * THREADS_PER_BLOCK.x;
+	const size_t SIZE = ROWS * COLUMNS * sizeof( real_t );
+	
+	// device storage for gpu computation
+	real_t* dev_m1 = 0;
+	real_t* dev_m2 = 0;
+	real_t* dev_mout = 0;
+	cudaMalloc( &dev_m1,  SIZE );
+	cudaMalloc( &dev_m2,  SIZE );
+	cudaMalloc( &dev_out, SIZE );
+	//host storage for reading the output of gpu computation
+	std::vector< real_t> host_mout( ROWS * COLUMNS );
+	
+	// host storage for cpu computation
+	std::vector< real_t > m1( ROWS * COLUMNS );
+	std::vector< real_t > m2( ROWS * COLUMNS );
+	std::vector< real_t > mout( ROWS * COLUMNS );
+
+    // initialize matrix with kernel; much faster than using
+    // for loops on the cpu
+	init_matrix<<<dim3( COLUMNS, ROWS ), 1>>>( dev_m1 );
+	init_matrix<<<dim3( COLUMNS, ROWS ), 1>>>( dev_m2 );
+
+    
+	// print upper 4x4 left corner of input matrix
+	std::cout << "INPUT MATRIX 1 - " << ROWS << " rows, " << COLUMNS << " columns" << std::endl;
+	print_matrix( &m[ 0 ], 4, 4, COLUMNS );
+	
+	// create events for timing execution
+	cudaEvent_t start = cudaEvent_t();
+	cudaEvent_t stop  = cudaEvent_t();
+	cudaEventCreate( &start );
+	cudaEventCreate( &stop  );
+    
+    // record time into start event 
+    cudaEventRecord( start, 0 ); // 0 is the default stream id
+	// execute kernel
+	transpose<<<BLOCKS, THREADS_PER_BLOCK>>>( dev_in, dev_out, ROWS, COLUMNS );
+	//transposeCoalesced<<<BLOCKS, THREADS_PER_BLOCK>>>>( dev_in, dev_out, COLUMNS, ROWS);
+    // issue request to record time into stop event
+    cudaEventRecord( stop, 0 );
+    // synchronize stop event to wait for end of kernel execution on stream 0
+    cudaEventSynchronize( stop );
+    // compute elapsed time (done by CUDA run-time) 
+	float elapsed = 0.f;
+	cudaEventElapsedTime( &elapsed, start, stop );
+	
+	std::cout << "Elapsed time (ms): " << elapsed / 1000 << std::endl;
+
+	// copy output data from device(gpu) to host(cpu)
+	cudaMemcpy( &outmatrix[ 0 ], dev_out, SIZE, cudaMemcpyDeviceToHost );
+
+	// print upper 4x4 corner of transposed matrix
+	std::cout << "\nOUTPUT MATRIX - " << COLUMNS << " rows, " << ROWS << " columns" << std::endl;
+	print_matrix( &outmatrix[ 0 ], 4, 4, ROWS );
+
+    // free memory
+    cudaFree( dev_in );
+    cudaFree( dev_out );
+
+    // release events
+    cudaEventDestroy( start );
+	cudaEventDestroy( stop  );
+
+
+	return 0;
+}
+
+
