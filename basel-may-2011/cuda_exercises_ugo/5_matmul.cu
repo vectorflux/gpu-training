e4fb19de67bd024018ea35750c4caee33af05249
uvaretto <uvaretto@45fdcf50-614b-4c32-8fc4-9530904cd465>
2011-05-05 07:47:42 +0000

    Added matmul without coalescing and cpu version

--- basel-may-2011/cuda_exercises_ugo/5_matmul.cu
+++ basel-may-2011/cuda_exercises_ugo/5_matmul.cu
@@ -22,14 +22,14 @@ __device__ real_t get_matrix_element( const real_t* m,
                                       int blockRow,
                                       int col,
                                       int row,
-                                      int num_col ) {
+                                      int num_columns ) {
                                       	
   
-    return m[ ( blockRow * blockDim.y + row ) * num_col + blockCol * blockDim.x + col ];
+    return m[ ( blockRow * blockDim.y + row ) * num_columns + blockCol * blockDim.x + col ];
 
 }
 
-__global__ void matmul( const real_t* m1, const real_t* m2, real_t* mout,
+__global__ void matmul_coalesced( const real_t* m1, const real_t* m2, real_t* mout,
                         int m1_rows, int m1_columns, int m2_columns  ) { // m1_columns == m2_rows
                                                                          // mout = m1_rows x m2_columns
 #ifdef AVOID_BANK_CONFLICTS	
@@ -39,15 +39,15 @@ __global__ void matmul( const real_t* m1, const real_t* m2, real_t* mout,
     __shared__ real_t M1[ TILE_SIZE ][ TILE_SIZE     ];
 	__shared__ real_t M2[ TILE_SIZE ][ TILE_SIZE + 1 ];     
 #endif		
-	const int blockRow = blockIdx.y * blockDim.y; 
-    const int blockCol = blockIdx.x * blockDim.x;
+	const int blockRow = blockIdx.y; 
+    const int blockCol = blockIdx.x;
     const int row = threadIdx.y;
     const int col = threadIdx.x;
     real_t out = 0.f;
     for( int b = 0; b != gridDim.x; ++b ) {
     	//copy data into shared memory
-    	M1[ row ][ col ] = get_matrix_element( m1, blockCol + b, blockRow, row, col );
-    	M2[ row ][ col ] = get_matrix_element( m2, blockCol, blockRow + b, row, col );
+    	M1[ row ][ col ] = get_matrix_element( m1, b, blockRow, row, col, m1_columns );
+    	M2[ row ][ col ] = get_matrix_element( m2, blockCol, b, row, col, m2_columns );
         __syncthreads(); // required to guarantee that data are computed before next step
                          // where a thread accesses data computed by other threads
         for( int k = 0; k != TILE_SIZE; ++k ) {
@@ -55,6 +55,36 @@ __global__ void matmul( const real_t* m1, const real_t* m2, real_t* mout,
         }
         __synchthreads(); // required to avoid that some threads start modifying
                           // data in cache before all threads have exited for loop    
-    }    
+    }
+
+   mout[ ( blockRow * blockDim.y + row ) * m2_columns + blockCol * blockDim.x + col ] = out;     
 }
 
+__global__ void matmul( const real_t* m1, const real_t* m2, real_t* mout,
+                        int m1_rows, int m1_columns, int m2_columns  ) { // m1_columns == m2_rows
+                                                                         // mout = m1_rows x m2_columns
+	const int row = blockIdx.y * blockDim.y + threadIdx.x; 
+    const int col = blockIdx.x * blockDim.x + threadIdx.y
+    const int row = threadIdx.y;
+    real_t out = m1[ row * m1_columns + 0 ] * m2[ 0 * m2_columns + col ];
+
+    for( int k = 1; k != m1_columns; ++k ) {
+    	out += m1[ row * m1_columns + k ] * m2[ k * m2_columns + col ];
+    }
+
+    mout[ row * m2_columns + col ] = out;
+}
+
+void matmul_ref( const real_t* m1, const real_t* m2, real_t* mout,
+                 int m1_rows, int m1_columns, int m2_columns  ) {
+                 	
+    for( int row = 0; row != m1_rows; ++row ) {
+    	for( int col = 0; col != m2_columns; ++col ) {
+    		for( int k = 0; k != m1_columns; ++k ) {
+    			mout[ row * m2_columns + col ] = m1[ row * m1_columns + k ] * m2[ k * m2_columns + col ];
+    		}
+    	}
+    }
+
+
+}
