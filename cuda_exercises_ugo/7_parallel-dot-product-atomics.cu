9271fb28a2e85d8327fa5c5401bec3d9d1d2d6a5
uvaretto <uvaretto@45fdcf50-614b-4c32-8fc4-9530904cd465>
2011-05-10 14:09:15 +0000

    Added error checking, comments and conditional compilation of serialized access

--- cuda_exercises_ugo/7_parallel-dot-product-atomics.cu
+++ cuda_exercises_ugo/7_parallel-dot-product-atomics.cu
@@ -9,20 +9,25 @@
 // #Rationale: shows how to perform the dot product of two vectors as a parallel reduction
 //             with all the computation on the GPU; last step is done through synchronized
 //             access to a shared variable.
+//             Kernel doesn't run on architecture < 2.0 (Fermi); return value stays set to zero. 
 // 
 // #Solution: store scalar products in local cache and iterate over cache elements
-//            performing incremental sums 
+//            performing incremental sums; perform last reduction on GPU through atomicAdd(float*...),
+//            only available on Fermi and above
 //
 // #Code: 1) compute launch grid configuration
 //        2) allocate data on host(cpu) and device(gpu)
 //        3) initialize data directly on GPU
 //        4) launch kernel
-//        5) read data back 
-//        6) free memory 
+//        5) report errors 
+//        6) read data back 
+//        7) free memory 
 //             
-// #Compilation: nvcc -arch=sm_20 4_parallel-dot-product-atomics.cu -o dot-product-atomics
+// #Compilation: nvcc -arch=sm_20 7_parallel-dot-product-atomics.cu -o dot-product-atomics
 //
 // #Execution: ./dot-product-atomics
+//
+// #Note: also check cudaMemset, cudaErrorString, cudaGetLastError usage
 //
 // #Note: as of CUDA 3.2 it seems that kernels do not stall anymore when invoking
 //        __syncthreads from within an if block dependent on the thread id;
@@ -34,7 +39,7 @@
 // #Note: -arch=sm_13 allows the code to run on every card available on Eiger and possibly even
 //        on students' laptops; it's the identifier for the architecture before Fermi (sm_20)
 //
-// #Note: -arch=sm_13 is the lowest architecture version that supports double precision
+// #Note: -arch=sm_20 is the lowest architecture version that allows to use floating point atomics
 //
 // #Note: the example can be extended to read configuration data and matrix size from the command line
 
@@ -42,6 +47,9 @@
 #include <vector>
 #include <iostream>
 #include <numeric>
+
+// comment following line to remove serialized access to shared data
+#define CORRECT_SYNC
 
 typedef float real_t;
 
@@ -61,8 +69,14 @@ __global__ void full_dot( const real_t* v1, const real_t* v2, real_t* out, int N
         __syncthreads();
         i /= 2; //not sure bitwise operations are actually faster
     }
-
-    if( threadIdx.x == 0 ) atomicAdd( out, cache[ 0 ] );//   out[ blockIdx.x ] = cache[ 0 ];
+#ifdef CORRECT_SYNC // sync access to shared data; serialized access
+    if( threadIdx.x == 0 ) atomicAdd( out, cache[ 0 ] );
+#else // no sync, what most likely happens is:
+      // 1) all threads read 0
+      // 2) all threads write concurrently 16 (local block dot product)
+    if( threadIdx.x == 0 ) *out += cache[ 0 ];
+#endif                
+    
 }
 
 real_t dot( const real_t* v1, const real_t* v2, int N ) {
@@ -88,7 +102,7 @@ int main(int argc, char** argv ) {
     
     const size_t ARRAY_SIZE = 1024;//1024 * 1024; //1Mi elements
     const int BLOCKS = 64;//512;
-    const int THREADS_PER_BLOCK = BLOCK_SIZE;//256; // total threads = 512 x 256 = 128ki threads;                                   // each thread spans 8 array elements  
+    const int THREADS_PER_BLOCK = BLOCK_SIZE;//256; // total threads = 512 x 256 = 128ki threads;                                   
     const size_t SIZE = ARRAY_SIZE * sizeof( real_t );
     
     // device storage
@@ -110,12 +124,15 @@ int main(int argc, char** argv ) {
     // initialize vector 2 with kernel; much faster than using for loops on the cpu
     init_vector<<< 1024, 256  >>>( dev_v2, ARRAY_SIZE );
     cudaMemcpy( &host_v2[ 0 ], dev_v2, SIZE, cudaMemcpyDeviceToHost );
-    // initialize result
+    
+    // initialize result on GPU: note the use of cudaMemset, alternatives are to run a kernel
+    // or copy from CPU
     cudaMemset( dev_out, 0, sizeof( real_t) );   
 
     // execute kernel
     full_dot<<<BLOCKS, THREADS_PER_BLOCK>>>( dev_v1, dev_v2, dev_out, ARRAY_SIZE );
-         
+    std::cout << cudaGetErrorString( cudaGetLastError() ) << std::endl;
+             
     // copy output data from device(gpu) to host(cpu)
     cudaMemcpy( &host_out, dev_out, sizeof( real_t ), cudaMemcpyDeviceToHost );
 
