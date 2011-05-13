22ddb6a7fc8f271eeadb44edd586ffd30b33de03
uvaretto <uvaretto@45fdcf50-614b-4c32-8fc4-9530904cd465>
2011-05-13 14:47:11 +0000

    Finished implementation of const mem access example

--- cuda_exercises_ugo/7_constant-memory.cu
+++ cuda_exercises_ugo/7_constant-memory.cu
@@ -1,57 +1,50 @@
-//WORK IN PROGRESS !!!!!
-
 // #CSCS CUDA Training 
 //
-// #Exercise 7 - sum vectors, fix number of threads per block, overlap communication and computation
+// #Exercise 7 - constant memory
 //
 // #Author Ugo Varetto
 //
-// #Goal: compute the sum of two vectors overlapping communication and computation
+// #Goal:  multiply input array elements by weights and write result into output array;
+//         investigate the use of constant memory for storing the weights and how
+//         having each thread in each warp access the same element compares
+//         to have each thread access a different element from a performance standpoint;
+//         also compare the result with storing data in global memory      
 //
-// #Rationale: using streams it is possible to subdivide computation and memory transfer
-//             operations in separate execution queue which can be executed in parallel;
-//             specifically it is possible to execute kernel computation while concurrently
-//             transferring data between host and device
+// #Rationale: constant memory can be used to store a small (64kiB) dataset frequently
+//             accessed from a kernel; behavior of constant memory is the opposite of
+//             global memory: it is faster to have multiple threads access the same
+//             element than having multiple threads access separate elements
 //
-// #Solution: subdivide domain into chunks, iterate over the array and at each
-//            iteration issue asynchronous calls to memcpy and kernels
-//            (always asynchronous) in separate streams. Note that
-//            on the GPU operations are split into one queue per operation type
-//            specifically: all copy operations from different streams go into the same
-//            queue in the same order as specified by client code, likewise all
-//            kernel invocations go into the same 'kernel invocation queue'.
-//            Now: if any copy operation (regardless of the associated stream) depends
-//            on e.g. a kernel execution then all subsequent copy operations in all streams must wait
-//            for the dependent copy operation to complete before they are executed; this means
-//            that client code is responsible for properly queueing operations to avoid conflicts. 
+// #Solution: implement and time different kernels:
+//            1) all the threads in a warp access the same element in the const array
+//            2) each threads in the grid accesses a different element in the const array
+//            redo the same for global memory timing the computation with events
 //
 // #Code: flow:
 //        1) compute launch grid configuration
 //        2) allocate data on host(cpu) and device(gpu)
-//        3) copy data from host ro device
-//        4) create streams
-//        5) iterate over array elements performing memory transfers and
-//           kernel invocation: at each iteration the number of elements
-//           being processed by separate streams is   VECTOR_CHUNK_SIZE x NUMBER_OF_STREAMS
-//        6) synchronize streams to wait for end of execution 
-//        7) consume data (in this case print result)
-//        8) free memory, streams and events (used to time operations)
+//        3) copy data from host ro device, in this case also copy to const global array on GPU
+//        4) launch and time kernels
+//        6) synchronize events to wait for end of execution 
+//        7) consume data (in this case print result and time)
+//        8) free memory and events (used to time operations)
 //        
-// #Compilation: [optimized] nvcc -arch=sm_13 2_2_sum-vectors-overlap.cu -o sum_vectors-overlap
-//               [wrong ordering, no overlap] nvcc -DSTREAM_NO_OVERLAP -arch=sm_13 2_2_sum-vectors-overlap.cu -o sum_vectors-overlap
-//                 
+// #Compilation: nvcc -arch=sm_13 7_constant-memory.cu -o constant-memory
 //
-// #Execution: ./sum-vectors-overlap
-//             note that you might experience some hysteresis! on Win 7 64bit, CUDA RC2 at each compilation
-//             it takes a few runs before the new ordering scheme becomes active!!! 
+// #Execution: ./constant-memory
 //
-// #Note: page locked memory required for async/stream operations
-//
+// #Note: not how on arch 1.3 devices broadcast access is faster than parallel access in constant
+//        memory (quite the opposite in global memory) because access to constant memory is serialized
+//        within half-warp: each half-warp can only access one element of const memory at a time     
+//       
 // #Note: the code is C++ also because the default compilation mode for CUDA is C++, all functions
 //        are named with C++ convention and the syntax is checked by default against C++ grammar rules 
 //
 // #Note: -arch=sm_13 allows the code to run on every card available on Eiger and possibly even
-//        on students' laptops; it's the identifier for the architecture before Fermi (sm_20)
+//        on students' laptops; it's the identifier for the architecture before Fermi (sm_20);
+//        in this case when double precision is used the number of elements in the arrays must
+//        be divided by two to allow for weights to fit in 64kiB of const memory.
+//
 // #Note: -arch=sm_13 is the lowest architecture version that supports double precision
 //
 // #Note: the example can be extended to read configuration data and array size from the command line and
@@ -69,79 +62,86 @@
 
 typedef float real_t;
 
-
-static const int NUM_WEIGHTS = 256;
-static const int NUMBER_OF_BLOCKS  = 2048;
-static const int THREADS_PER_BLOCK = NUM_WEIGHTS;
 static const int HALF_WARP = 16;
+static const int NUMBER_OF_BLOCKS  = 64;
+static const int THREADS_PER_BLOCK = 16 * HALF_WARP;
+static const int VECTOR_SIZE = THREADS_PER_BLOCK * NUMBER_OF_BLOCKS;
+static const int NUM_WEIGHTS = VECTOR_SIZE;
+static const int BYTE_SIZE = sizeof( real_t ) * VECTOR_SIZE;
+
+// number of weights must fit in local memory = 64kiB
+// 64 blocks x ( 16 x 16 threads per block ) = 16ki elements x 4 bytes per element = 64kiB   
 
 __constant__ real_t weights[ NUM_WEIGHTS ];
 
 
-// out[ global thread id ] = in[ global thread id ] * weights[ block id ];
-// each thread in the group accesses the same element in the constant weight array,
-// each read from 16(half warp) adjacent threads result in a single transfer operation
-__global__ void weight_mul_broadcast( const real_t* vin, real_t* out, int num_elements ) {
+// out[ global thread id ] = in[ global thread id ] x weights[ block offset + half warp id ];
+// each thread in the half-warp accesses the same element in the constant weight array
+__global__ void weight_mul_broadcast( const real_t* vin, real_t* out ) {
     // compute current thread id
-    const int xIndex = blockIdx.x * blockDim.x + threadIdx.x;          
-    // since we assume that num threads >= num element we need to make sure we do note write outside the
-    // range of the output buffer 
-    if( xIndex < num_elements ) out[ xIndex ] = vin[ xIndex ] * weights[ threadIdx.x / HALF_WARP ];
+    const int xBlock = blockIdx.x * blockDim.x;
+    const int xIndex = xBlock + threadIdx.x;          
+    out[ xIndex ] = vin[ xIndex ] * weights[ blockIdx.x + threadIdx.x / 16 ];
 }
 
-// out[ global thread id ] = in[ global thread id ] * weights[ local thread id ];
-// each thread in the group accesses a different weight: each access from half warp
+// out[ global thread id ] = in[ global thread id ] x weights[ global thread id ];
+// each thread  accesses a different weight: each access from half warp
 // threads is serialized i.e. it will take 16 separate read operations to fill a group of 16 output
 // elements as comparaed to 16 parallel transfers or less(when coalesced) in the case of global
 // memory 
-__global__ void weight_mul_serial( const real_t* vin, real_t* out, int num_elements ) {
+__global__ void weight_mul_separate( const real_t* vin, real_t* out ) {
     // compute current thread id
-    const int xIndex = blockIdx.x * blockDim.x + threadIdx.x;          
-    // since we assume that num threads >= num element we need to make sure we do note write outside the
-    // range of the output buffer 
-    if( xIndex < num_elements ) out[ xIndex ] = vin[ xIndex ] * weights[ threadIdx.x ];
+    const int xBlock = blockIdx.x * blockDim.x;
+    const int xIndex = xBlock + threadIdx.x;              
+    out[ xIndex ] = vin[ xIndex ] * weights[ xBlock + threadIdx.x ];
 }
 
 
-// out[ global thread id ] = in[ global thread id ] * weights[ local thread id ];
-// each thread in the group accesses a different weight: each access from half warp
-// threads is serialized i.e. it will take 16 separate read operations to fill a group of 16 output
-// elements as comparaed to 16 parallel transfers or less(when coalesced) in the case of global
-// memory 
-__global__ void weight_mul_global( const real_t* vin, const real_t* weights, real_t* out, int num_elements ) {
+// same as weight_mul_parallel but reading weights from global memory
+__global__ void weight_mul_global_separate( const real_t* vin, const real_t* w, real_t* out ) {
     // compute current thread id
-    const int xIndex = blockIdx.x * blockDim.x + threadIdx.x;          
-    // since we assume that num threads >= num element we need to make sure we do note write outside the
-    // range of the output buffer 
-    if( xIndex < num_elements ) out[ xIndex ] = vin[ xIndex ] * weights[ threadIdx.x ];
+    const int xBlock = blockIdx.x * blockDim.x;
+    const int xIndex = xBlock + threadIdx.x;              
+    out[ xIndex ] = vin[ xIndex ] * w[ xBlock + threadIdx.x ];
 }
 
+// same as weight_mul_broadcast but reading weights from global memory
+__global__ void weight_mul_global_bcast( const real_t* vin, const real_t* w, real_t* out ) {
+    // compute current thread id
+    const int xBlock = blockIdx.x * blockDim.x;
+    const int xIndex = xBlock + threadIdx.x;              
+    out[ xIndex ] = vin[ xIndex ] * w[  xBlock + threadIdx.x / 16 ];//+ threadIdx.x / HALF_WARP ];
+}
 
-// generate constant element
-    struct GenSeq {
-        static int v_; 
-        GenSeq( real_t v )  { v_ = v; }
-        real_t operator()() const { return ++v_; }
-    };
+// generate sequence {i,i+1,i+2,...}
+struct GenSeq {
+    static int v_; 
+    GenSeq( real_t v )  { v_ = v; }
+    real_t operator()() const { return v_++; }
+};
+int GenSeq::v_ = 0;
 
-    int GenSeq::v_ = 0;
 
 //------------------------------------------------------------------------------
 int main( int , char**  ) {
-      
-    const int VECTOR_SIZE = NUMBER_OF_BLOCKS * THREADS_PER_BLOCK;
-    const int BYTE_SIZE = sizeof( real_t ) * VECTOR_SIZE;
-      
+         
     // host allocated storage; page locked memory required for async/stream operations
     std::vector< real_t > v( VECTOR_SIZE, 1.f );
     std::vector< real_t > host_w( NUM_WEIGHTS );
     std::vector< real_t > vout( VECTOR_SIZE );
    
-    std::generate( v.begin(), v.end(), GenSeq( 0.0f ) );
-
+    std::generate( host_w.begin(), host_w.end(), GenSeq( 0.0f ) );
+    std::cout << "Input: ";
+    std::copy( v.begin(), v.begin() + 10, std::ostream_iterator< real_t >( std::cout, ", ") ); 
+    std::cout << " ..." << std::endl;
+    std::cout << "Weigths: ";
+    std::copy( host_w.begin(), host_w.begin() + 10, std::ostream_iterator< real_t >( std::cout, ", ") ); 
+    std::cout << " ..." << std::endl;
+ 
+    //upload data to const global on GPU (data are const on the GPU, must be initialized from the CPU)
     cudaMemcpyToSymbol( weights, &host_w[ 0 ], sizeof( real_t ) * NUM_WEIGHTS );
-
-    // gpu allocated storage: number of arrays == number of streams == 2
+ 
+    // gpu allocated storage
     real_t* dev_vin  = 0;
     real_t* dev_vout = 0;
     real_t* dev_w    = 0;
@@ -149,34 +149,66 @@ int main( int , char**  ) {
     cudaMalloc( &dev_vin,  BYTE_SIZE );
     cudaMalloc( &dev_vout, BYTE_SIZE );
     cudaMalloc( &dev_w, sizeof( real_t) * NUM_WEIGHTS );
+
+    cudaMemcpy( dev_vin, &v[ 0 ], BYTE_SIZE, cudaMemcpyHostToDevice );
     
+    cudaMemcpy( dev_w,   &host_w[ 0 ], sizeof( real_t ) * NUM_WEIGHTS, cudaMemcpyHostToDevice );
+   
     // events; for timing
     cudaEvent_t start = cudaEvent_t();
     cudaEvent_t stop  = cudaEvent_t();
     cudaEventCreate( &start );
     cudaEventCreate( &stop );
 
-    cudaEventRecord( start, 0 );
-    weight_mul_broadcast<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_vout, VECTOR_SIZE );
-    cudaEventRecord( stop, 0 );
-    cudaEventSynchronize( stop );
     float e = float();
-    cudaEventElapsedTime( &e, start, stop );
-    std::cout << "Broadcast:  " << e << " ms" << std::endl;
 
     cudaEventRecord( start, 0 );
-    weight_mul_serial<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_vout, VECTOR_SIZE );
+    weight_mul_broadcast<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_vout );
+    cudaEventRecord( stop, 0 );
+    cudaEventSynchronize( stop );    
+    cudaEventElapsedTime( &e, start, stop );
+    std::cout << "Const broadcast:  " << e << " ms" << std::endl;
+    //copy data from GPU and print result
+    cudaMemcpy( &vout[ 0 ], dev_vout, BYTE_SIZE, cudaMemcpyDeviceToHost );
+    std::cout << "Result: ";
+    std::copy( vout.begin(), vout.begin() + 48, std::ostream_iterator< real_t >( std::cout, ", ") ); 
+    std::cout << " ...\n" << std::endl;
+
+    cudaEventRecord( start, 0 );
+    weight_mul_separate<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_vout );
     cudaEventRecord( stop, 0 );
     cudaEventSynchronize( stop );
     cudaEventElapsedTime( &e, start, stop );
-    std::cout << "Serialized: " << e << " ms" << std::endl;
+    std::cout << "Const separate: " << e << " ms" << std::endl;
+     //copy data from GPU and print result
+    cudaMemcpy( &vout[ 0 ], dev_vout, BYTE_SIZE, cudaMemcpyDeviceToHost );
+    std::cout << "Result: ";
+    std::copy( vout.begin(), vout.begin() + 48, std::ostream_iterator< real_t >( std::cout, ", ") ); 
+    std::cout << " ...\n" << std::endl;
 
     cudaEventRecord( start, 0 );
-    weight_mul_global<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_w, dev_vout, VECTOR_SIZE );
+    weight_mul_global_bcast<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_w, dev_vout );
     cudaEventRecord( stop, 0 );
     cudaEventSynchronize( stop );
     cudaEventElapsedTime( &e, start, stop );
-    std::cout << "Global:     " << e << " ms" << std::endl;
+    std::cout << "Global broadcast:     " << e << " ms" << std::endl;
+    //copy data from GPU and print result
+    cudaMemcpy( &vout[ 0 ], dev_vout, BYTE_SIZE, cudaMemcpyDeviceToHost );
+    std::cout << "Result: ";
+    std::copy( vout.begin(), vout.begin() + 48, std::ostream_iterator< real_t >( std::cout, ", ") ); 
+    std::cout << " ...\n" << std::endl;
+  
+    cudaEventRecord( start, 0 );
+    weight_mul_global_separate<<< NUMBER_OF_BLOCKS, THREADS_PER_BLOCK >>>( dev_vin, dev_w, dev_vout );
+    cudaEventRecord( stop, 0 );
+    cudaEventSynchronize( stop );
+    cudaEventElapsedTime( &e, start, stop );
+    std::cout << "Global separate:     " << e << " ms" << std::endl;
+    //copy data from GPU and print result
+    cudaMemcpy( &vout[ 0 ], dev_vout, BYTE_SIZE, cudaMemcpyDeviceToHost );
+    std::cout << "Result: ";
+    std::copy( vout.begin(), vout.begin() + 48, std::ostream_iterator< real_t >( std::cout, ", ") ); 
+    std::cout << " ...\n" << std::endl;
 
     // free memory
     cudaFree( dev_vin  );
