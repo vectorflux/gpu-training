be06e8dc5efa6310f2eda707db791aed22559ce4
uvaretto <uvaretto@45fdcf50-614b-4c32-8fc4-9530904cd465>
2011-05-30 09:41:46 +0000

    Updated declarations of shared memory globals

--- trunk/cuda_exercises_ugo/6_matmul-dynamic-shared-mem.cu
+++ trunk/cuda_exercises_ugo/6_matmul-dynamic-shared-mem.cu
@@ -67,7 +67,7 @@ __device__ real_t get_matrix_element( const real_t* m, //matrix
 //                declared as a global variable; the size of such buffer
 //                is specified at kernel launch as the third parameter
 //                in the <<< >>> operator
-extern __shared__ real_t cache[];
+__shared__ real_t* cache;
 
 // compute block matrix multiply:
 // - matrix block size == tile size == CUDA thread block size
