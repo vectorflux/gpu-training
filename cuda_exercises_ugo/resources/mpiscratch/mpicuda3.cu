da2203aaad5818a17315dde3be530680483fe791
uvaretto <uvaretto@45fdcf50-614b-4c32-8fc4-9530904cd465>
2011-07-20 15:32:42 +0000

    Added cuda mpi with timing

--- cuda_exercises_ugo/resources/mpiscratch/mpicuda3.cu
+++ cuda_exercises_ugo/resources/mpiscratch/mpicuda3.cu
@@ -1,4 +1,4 @@
-// "mpi + cuda reduction" 
+// "mpi + cuda reduction + timing" 
 
 #ifdef GPU
 #include <cuda.h>
@@ -13,6 +13,7 @@
 #include <string>
 #include <set>
 #include <numeric>
+#include <ctime>
 
 // switches:
 // #GPU : enable GPU computation
@@ -20,6 +21,7 @@
 // #REDUCE_CPU: perform final per-task reduction step on the CPU
 // #DOUBLE_: double precision
 // #MPI_RROBIN_: assume a round robin layout i.e process 0 -> node 0, process 1 -> node 1 ...
+// #NO_GPU_MALLOC_TIME: do not take into account malloc time; usually this part of an initialization step
 
 
 // compilation with mvapich2:
@@ -60,7 +62,7 @@ typedef double real_t;
 
 //------------------------------------------------------------------------------
 #ifdef GPU
-const int BLOCK_SIZE = 256;
+const int BLOCK_SIZE = 512;
 #ifndef DOUBLE_ //atomics are available for single precision only!!!
 __global__ void dot_product_kernel( const real_t* v1, const real_t* v2, int N, real_t* out ) {
     __shared__ real_t cache[ BLOCK_SIZE ];
@@ -148,7 +150,7 @@ int main( int argc, char** argv ) {
     //std::vector< int > sendbuf( numtasks, node_count );
     // MPI Scatter parameters: address of send buffer,
     //                         per-receiving process receive buffer size,...
-    // send buffer size = num tasks x per-reeiveing-process buffer size
+    // send buffer size = num tasks x per-receiving-process buffer size
     //MPI_( MPI_Scatter( &sendbuf[ 0 ],  1, MPI_INT, &node_count, 1, MPI_INT, 0, MPI_COMM_WORLD ) ); 
     // Option 2: simply broadcast the number of nodes
     MPI_( MPI_Bcast( &node_count, 1, MPI_INT, 0, MPI_COMM_WORLD ) );
@@ -171,11 +173,15 @@ int main( int argc, char** argv ) {
         v2[ i ] = 1;  
     }
 
+    std::vector< double > begins( numtasks );
+    std::vector< double > ends( numtasks );
+    double begin = clock();
+    MPI_( MPI_Gather( &begin, 1, MPI_DOUBLE, &begins[ 0 ], 1, MPI_DOUBLE, 0, MPI_COMM_WORLD ) ); 
+
     // PARALLEL DOT PRODUCT COMPUTATION
     real_t partial_dot = 0.f;
 #ifndef GPU
     int t = 0;
-    int p = 0;
     for( t = 0; t != PER_MPI_TASK_ARRAY_SIZE; ++t ) {
         partial_dot += v1[ t ] * v2[ t ];
     }
@@ -212,6 +218,9 @@ int main( int argc, char** argv ) {
         MPI_( MPI_Abort( MPI_COMM_WORLD, 1 ) );
         return 1;
     }
+#ifdef NO_GPU_MALLOC_TIME
+    double malloc_begin = clock();
+#endif
     real_t* dev_v1   = 0;
     real_t* dev_v2   = 0;
     real_t* dev_dout = 0;
@@ -225,6 +234,10 @@ int main( int argc, char** argv ) {
         MPI_( MPI_Abort( MPI_COMM_WORLD, 1 ) );
         return 1;
     }
+#ifdef NO_GPU_MALLOC_TIME
+    double malloc_end = clock();
+    begin += malloc_end - malloc_begin;
+#endif
     // MOVE DATA TO GPU
     if( cudaMemcpy( dev_v1, &v1[ 0 ], sizeof( real_t ) * PER_MPI_TASK_ARRAY_SIZE,
                     cudaMemcpyHostToDevice ) != cudaSuccess ) {
@@ -243,7 +256,10 @@ int main( int argc, char** argv ) {
     const int NUM_BLOCKS = std::min( PER_MPI_TASK_ARRAY_SIZE  / NUM_THREADS_PER_BLOCK,
                                      0xffff ); // max number of blocks is 64k 
     
-#ifndef REDUCE_CPU
+#ifndef REDUCE_CPU 
+#ifdef NO_GPU_MALLOC_TIME
+    malloc_begin = clock();
+#endif
     if( cudaMalloc( &dev_dout, sizeof( real_t ) * 1 ) != cudaSuccess ) {
         std::cerr << task << ' ' << __LINE__ << ' ' << cudaGetErrorString( cudaGetLastError() ) <<  " cudaMalloc FAILED\n"; 
         MPI_( MPI_Abort( MPI_COMM_WORLD, 1 ) );
@@ -255,6 +271,10 @@ int main( int argc, char** argv ) {
         MPI_( MPI_Abort( MPI_COMM_WORLD, 1 ) );
         return 1;
     }
+#ifdef NO_GPU_MALLOC_TIME
+    malloc_end = clock();
+    begin += malloc_end - malloc_begin;
+#endif
     // actual on-device computation    
     dot_product_kernel<<<NUM_BLOCKS, NUM_THREADS_PER_BLOCK>>>( dev_v1, dev_v2, PER_MPI_TASK_ARRAY_SIZE, dev_dout ); 
     // check for kernel launch errors: it is not possible to catch on-device execution errors but only
@@ -292,9 +312,17 @@ int main( int argc, char** argv ) {
     real_t result = 0.;
     MPI_( MPI_Reduce( &partial_dot, &result, 1, MPI_REAL_T_, MPI_SUM, 0, MPI_COMM_WORLD ) );
 
+    double end = clock();
+    MPI_( MPI_Gather( &end, 1, MPI_DOUBLE, &ends[ 0 ], 1, MPI_DOUBLE, 0, MPI_COMM_WORLD ) ); 
+
+    const std::pair< double, double > minmax( *std::min_element( begins.begin(), begins.end() ),
+                                              *std::max_element( ends.begin(), ends.end() ) );  
+
+
     // IF RANK == 0 -> PRINT RESULT
     if( task == 0 ) {
         std::cout << "dot product result: " << result << std::endl;
+        std::cout << "time: " << ( minmax.second - minmax.first ) / CLOCKS_PER_SEC << 's' << std::endl;
     } 
   
 #ifdef GPU
